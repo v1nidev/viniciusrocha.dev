4b79076b5baf8b6bc684bfaa4e3fb187bcae35e1
Vinicius Rocha <mvinicius.gbo@gmail.com>
2021-10-22 16:44:51 -0300

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/fonts/");
+  
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
