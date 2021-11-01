395114f8d984430e04ce658bd51887ebc1d31482
Vinicius Rocha <mvinicius.gbo@gmail.com>
2021-11-01 13:58:04 -0300

    Fix page not refreshing after shortcode change

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const shortcodes = require('./src/_includes/shortcodes')
+const shortcodes = getShortcodes()
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/fonts/");
@@ -22,3 +22,25 @@ module.exports = function (eleventyConfig) {
     },
   };
 };
+
+function getShortcodes() {
+  return [
+    function p(content) {
+      return `
+      <p class="text-xs md:text-sm lg:text-base">
+        ${content}
+      </p>
+    `
+    },
+
+    function sectTitle(pair) {
+      return `
+      <h2 class="text-lg xl:text-5xl tracking-wider text-gray-500 leading-snug xl:leading-normal">
+        ${pair[0] ? pair[0] : ''}
+        <br>
+        <span class="text-4xl xl:text-8xl font-bold">${pair[1] || pair[1]}</span>
+      </h2>
+    `
+    }
+  ]
+}
--- src/_includes/shortcodes.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  function p(content) {
-    return `
-      <p class="text-xs md:text-sm lg:text-base">
-        ${content}
-      </p>
-    `
-  }
-]
